556f818706430aea1be0ec9139caa32f1c42785e
Bharath-varma-p <cogs132@gmail.com>
2021-10-10 11:17:58 +0530

    hamburger menu removed 1.1

--- Stats1.py
+++ Stats1.py
@@ -7,7 +7,7 @@ hide_menu_style = """
         #MainMenu {visibility: hidden;}
         </style>
         """
-st.markdown(hide_menu_style, unsafe_allow_html=True)
+st.markdown(hide_menu_style, unsafe_allow_html=True)   #removed hamburger menu
 
 st.title('Uber pickups in NYC')
 
